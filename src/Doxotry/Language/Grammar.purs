e9e175a6ef2a664c6744c6e28b580c92f9568784
Henry Blanchette <henry@rybl.net>
2026-02-07 16:21:31 -0500

    simple type checker

--- src/Doxotry/Language/Grammar.purs
+++ src/Doxotry/Language/Grammar.purs
@@ -2,49 +2,106 @@ module Doxotry.Language.Grammar where
 
 import Prelude
 
+import Data.Eq.Generic (genericEq)
+import Data.Generic.Rep (class Generic)
+import Data.List (List)
+import Data.Newtype (class Newtype)
+import Data.Show.Generic (genericShow)
 import Data.Tuple.Nested (type (/\))
 
 --------------------------------------------------------------------------------
 
 data Ty
-  = NumberTy
-  | StringTy
-  | VarTy VarTy
+  = BaseTy BaseTy
+  | FunTy FunTy
 
-type VarTy = { var :: TyVar }
+type BaseTy = { base :: TyBase }
+
+type FunTy = { prm :: TmVar, dom :: Ty, cod :: Ty }
+
+derive instance Generic Ty _
+
+instance Show Ty where
+  show x = genericShow x
+
+instance Eq Ty where
+  eq x = genericEq x
+
+data TyBase
+  = NumberTyBase
+  | StringTyBase
+
+derive instance Generic TyBase _
+
+instance Show TyBase where
+  show x = genericShow x
+
+derive instance Eq TyBase
 
 --------------------------------------------------------------------------------
 
-data Tm ty
-  = LitTm (LitTm ty)
-  | VarTm (VarTm ty)
-  | AppTm (AppTm ty)
-  | FunTm (FunTm ty)
-  | CloTm (CloTm ty)
+data Tm an
+  = LitTm LitTm an
+  | VarTm VarTm an
+  | AppTm (AppTm an) an
+  | FunTm (FunTm an) an
+  | CloTm (CloTm an) an
 
-type LitTm ty = { lit :: Lit, ty :: ty }
+derive instance Generic (Tm an) _
 
-type VarTm ty = { var :: TmVar, ty :: ty }
+instance Show an => Show (Tm an) where
+  show x = genericShow x
 
-type AppTm ty = { apl :: Tm ty, arg :: Tm ty, ty :: ty }
+derive instance Eq an => Eq (Tm an)
 
-type FunTm ty = { prm :: TmVar, body :: Tm ty, ty :: ty }
+type LitTm = { lit :: TmLit }
 
-type CloTm ty = { env :: ExeEnv ty, body :: Tm ty, ty :: ty }
+type VarTm = { var :: TmVar }
+
+type AppTm an = { apl :: Tm an, arg :: Tm an }
+
+type FunTm an = { prm :: TmVar, dom :: Ty, body :: Tm an }
+
+type CloTm an = { env :: ExeEnv an, body :: Tm an }
 
 --------------------------------------------------------------------------------
 
-newtype TyCtx = Ctx (Array (String /\ Ty))
+newtype TyCtx = TyCtx (List (TmVar /\ Ty))
 
-newtype ExeEnv ty = Env (Array (String /\ Tm ty))
+derive instance Newtype TyCtx _
+
+derive newtype instance Show TyCtx
+
+derive newtype instance Eq TyCtx
+
+newtype ExeEnv an = Env (List (String /\ Tm an))
+
+derive newtype instance Show an => Show (ExeEnv an)
+
+derive newtype instance Eq an => Eq (ExeEnv an)
 
 newtype TmVar = TmVar String
 
+derive newtype instance Show TmVar
+
+derive newtype instance Eq TmVar
+
 newtype TyVar = TyVar String
+
+derive newtype instance Show TyVar
+
+derive newtype instance Eq TyVar
 
 --------------------------------------------------------------------------------
 
-data Lit
-  = NumberLit Number
-  | StringLit String
+data TmLit
+  = NumberTmLit Number
+  | StringTmLit String
+
+derive instance Eq TmLit
+
+derive instance Generic TmLit _
+
+instance Show TmLit where
+  show x = genericShow x
 
--- src/Doxotry/Language/Typing.purs
+++ src/Doxotry/Language/Typing.purs
@@ -2,9 +2,14 @@ module Doxotry.Language.Typing where
 
 import Prelude
 
-import Doxotry.Language.Grammar
-import Control.Monad.Reader (class MonadReader)
-import Doxotry.Utility (todo)
+import Control.Alternative (guard)
+import Control.Monad.Error.Class (class MonadThrow, throwError)
+import Control.Monad.Reader (class MonadReader, ask, local)
+import Data.List as List
+import Data.Maybe (maybe)
+import Data.Newtype (over, unwrap)
+import Data.Tuple.Nested (type (/\), (/\))
+import Doxotry.Language.Grammar (Tm(..), TmLit(..), TmVar, Ty(..), TyBase(..), TyCtx(..))
 
 --------------------------------------------------------------------------------
 
@@ -12,21 +17,72 @@ type Ctx = { tyCtx :: TyCtx }
 
 type Env = {}
 
---------------------------------------------------------------------------------
-
--- TODO: collect subtyping constraints
-
-check :: forall m ty. MonadReader Ctx m => Ty -> Tm ty -> m (Tm Ty)
-check ty (LitTm tm) = do
-  tm' <- checkLit ty tm.lit
-  pure $ LitTm tm'
-check ty (VarTm tm) = todo "" -- TODO: assert subtyping constraint of ascribed type of var to expected type
-check ty (AppTm tm) = todo ""
-check ty (FunTm tm) = todo ""
-check ty (CloTm tm) = todo ""
+newtype TypeError = TypeError
+  { message :: String
+  }
 
 --------------------------------------------------------------------------------
 
-checkLit :: forall m ty. Monad m => Ty -> Lit -> m (LitTm ty)
-checkLit _ _ = todo "checkLit"
+type CheckedTm an = Tm (Ty /\ an)
+
+checkTm
+  :: forall m an
+   . MonadReader Ctx m
+  => MonadThrow TypeError m
+  => Show an
+  => Ty
+  -> Tm an
+  -> m (CheckedTm an)
+checkTm ty@(BaseTy { base: StringTyBase }) (LitTm tl@({ lit: StringTmLit _ }) an) = pure $ LitTm tl (ty /\ an)
+checkTm ty@(BaseTy { base: NumberTyBase }) (LitTm tl@({ lit: NumberTmLit _ }) an) = pure $ LitTm tl (ty /\ an)
+checkTm ty tm0@(VarTm tm an) = do
+  ty' <- getTypeOfTmVar tm.var
+  unless (ty == ty') do
+    throwError $ TypeError { message: "var " <> show tm0 <> " was expected to have type " <> show ty <> ", but it actually has type " <> show ty' }
+  pure $ VarTm { var: tm.var } (ty /\ an)
+checkTm ty (AppTm tm an) = do
+  apl <- case tm.apl of
+    FunTm apl _ -> pure apl
+    apl -> throwError $ TypeError { message: "the applicant of an application must be a function term, but it actually was " <> show apl }
+  arg' <- checkTm apl.dom tm.arg
+  body' <- checkTm ty apl.body
+  pure $ AppTm
+    { apl:
+        FunTm
+          { prm: apl.prm, dom: apl.dom, body: body' }
+          (FunTy { prm: apl.prm, dom: apl.dom, cod: ty } /\ an)
+    , arg: arg'
+    }
+    (ty /\ an)
+checkTm ty tm0@(FunTm tm an) = do
+  funTy <- case ty of
+    FunTy funTy -> pure funTy
+    _ -> throwError $ TypeError { message: "The term " <> show tm0 <> " was expected to have a non-function type " <> show ty <> ", but it is actually a function term" }
+  unless (funTy.dom == tm.dom) do
+    throwError $ TypeError { message: "The term " <> show tm <> " was expected to be a function term with domain " <> show funTy.dom <> ", but it actually had domain " <> show tm.dom }
+  body' <-
+    extendTyCtx tm.prm tm.dom do
+      checkTm funTy.cod tm.body
+  pure $
+    FunTm
+      { prm: tm.prm, dom: tm.dom, body: body' }
+      (ty /\ an)
+checkTm ty tm = throwError $ TypeError { message: "The term " <> show tm <> " was expected to have type " <> show ty <> ", but it can't have that type." }
+
+extendTyCtx :: forall m a. MonadReader Ctx m => TmVar -> Ty -> m a -> m a
+extendTyCtx x ty ma = local (\ctx -> ctx { tyCtx = ctx.tyCtx # over TyCtx (List.Cons (x /\ ty)) }) ma
+
+--------------------------------------------------------------------------------
+
+getTypeOfTmVar :: forall m. MonadReader Ctx m => MonadThrow TypeError m => TmVar -> m Ty
+getTypeOfTmVar x = do
+  ctx <- ask
+  ctx.tyCtx
+    # unwrap
+    # List.findMap
+        ( \(x' /\ ty) -> do
+            guard $ x == x'
+            pure ty
+        )
+    # maybe (throwError $ TypeError { message: "Could not find a variable in context of the name " <> show x }) pure
 
